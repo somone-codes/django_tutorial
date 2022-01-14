51f70ca50c58bc31d92b87c96caca13e0d0d0547
somone-codes <someone_mails@protonmail.com>
2022-01-14 20:40:25 +0530

    Extending UserRegistrationForm to included email field

--- users/views.py
+++ users/views.py
@@ -1,16 +1,17 @@
+from users.forms import UserRegistrationForm
+
 from django.shortcuts import render, redirect
 from django.http import HttpRequest
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 
 
 def register(request: HttpRequest):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = UserRegistrationForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get("username")
             messages.success(request, f"user {username} registered successfully")
             return redirect("blog-home")
     else:
-        return render(request, "users/html/register.html", {"form": UserCreationForm()})
+        return render(request, "users/html/register.html", {"form": UserRegistrationForm()})
