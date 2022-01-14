96365bb02c85564eb0b3f069164a839ad9abb60a
somone-codes <someone_mails@protonmail.com>
2022-01-14 20:39:55 +0530

    Added user registration

--- django_tutorial/urls.py
+++ django_tutorial/urls.py
@@ -1,9 +1,12 @@
 """django_tutorial URL Configuration
 """
+
+from users.views import register
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', include('blog.urls')),
+    path("", include('blog.urls')),
+    path("register/", register, name="register"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpRequest
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
-# Create your views here.
+
+def register(request: HttpRequest):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(request, f"user {username} registered successfully")
+            return redirect("blog-home")
+    else:
+        return render(request, "users/html/register.html", {"form": UserCreationForm()})
