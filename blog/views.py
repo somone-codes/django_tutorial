c82e59c069d1160199f4eccae8819b6b3e5ba193
somone-codes <someone_mails@protonmail.com>
2022-01-14 18:55:15 +0530

    Fetch posts from DB

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,13 @@
+from .models import Post
+
 from django.shortcuts import render
 from django.http import HttpRequest
-
-posts = [
-    {
-        "author": "Someone",
-        "title": "Obligatory First post",
-        "created_on": "14-01-2022",
-        "content": "Hello there, this is my first post ever!"
-    },
-    {
-        "author": "Someone 2",
-        "title": "Following up on the first post",
-        "created_on": "14-01-2022",
-        "content": "Hit me up, lets chat."
-    }
-]
 
 
 def home(request: HttpRequest):
     context = {
         "title": "Welcome to blog's",
-        "posts": posts
+        "posts": Post.objects.all()
     }
     return render(request, "blog/html/home.html", context)
 
