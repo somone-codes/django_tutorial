882885dcadab0046232c4c94c60a84d36ecf4bbb
somone-codes <someone_mails@protonmail.com>
2022-01-15 10:38:40 +0530

    Added login/logout and register capability.

--- django_tutorial/urls.py
+++ django_tutorial/urls.py
@@ -4,9 +4,12 @@
 from users.views import register
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include('blog.urls')),
     path("register/", register, name="register"),
+    path("login/", LoginView.as_view(template_name="users/html/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(template_name="users/html/logout.html"), name="logout"),
 ]
--- users/views.py
+++ users/views.py
@@ -10,8 +10,7 @@ def register(request: HttpRequest):
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get("username")
-            messages.success(request, f"user {username} registered successfully")
-            return redirect("blog-home")
+            messages.success(request, "Account has been created successfully. Please login.")
+            return redirect("login")
     else:
         return render(request, "users/html/register.html", {"form": UserRegistrationForm()})
