9e824c60f0945bf502e9447adf7462f8978921d3
somone-codes <someone_mails@protonmail.com>
2022-01-15 22:07:36 +0530

    Ability to add posts

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 urlpatterns = [
     path("", views.PostListView.as_view(), name="blog-home"),
     path("post/<int:pk>/", views.PostDetailView.as_view(), name="post-detail"),
+    path("post/create/", views.PostCreateView.as_view(), name="post-create"),
     path("about", views.about, name="blog-about"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from .models import Post
 
 from django.shortcuts import render
 from django.http import HttpRequest
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 
 
 def about(request: HttpRequest):
@@ -21,3 +21,12 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
     model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
