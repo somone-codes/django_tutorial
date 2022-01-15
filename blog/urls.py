f3c935615662a09d9e518e76aa4e130db8d85ab8
somone-codes <someone_mails@protonmail.com>
2022-01-15 22:12:31 +0530

    Ability to update posts and ensure user is logged in to update

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 urlpatterns = [
     path("", views.PostListView.as_view(), name="blog-home"),
     path("post/<int:pk>/", views.PostDetailView.as_view(), name="post-detail"),
+    path("post/<int:pk>/update", views.PostUpdateView.as_view(), name="post-update"),
     path("post/create/", views.PostCreateView.as_view(), name="post-create"),
     path("about", views.about, name="blog-about"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,8 @@ from .models import Post
 
 from django.shortcuts import render
 from django.http import HttpRequest
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 def about(request: HttpRequest):
@@ -12,18 +13,27 @@ def about(request: HttpRequest):
     return render(request, "blog/html/about.html", context)
 
 
-class PostListView(ListView):
+class PostListView(LoginRequiredMixin, ListView):
     model = Post
     context_object_name = "posts"
     template_name = "blog/html/home.html"
     ordering = ["-created_on"]
 
 
-class PostDetailView(DetailView):
+class PostDetailView(LoginRequiredMixin, DetailView):
     model = Post
 
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
     model = Post
     fields = ['title', 'content']
 
