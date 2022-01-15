5ef8555acacc17d5ff217cd21c380f6b32fd2eb1
somone-codes <someone_mails@protonmail.com>
2022-01-15 12:08:15 +0530

    Profile view with profile picture.

--- django_tutorial/urls.py
+++ django_tutorial/urls.py
@@ -5,12 +5,18 @@ from users.views import register, profile
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.views import LoginView, LogoutView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include('blog.urls')),
     path("register/", register, name="register"),
-    path("profile/",profile, name="profile"),
+    path("profile/", profile, name="profile"),
     path("login/", LoginView.as_view(template_name="users/html/login.html"), name="login"),
     path("logout/", LogoutView.as_view(template_name="users/html/logout.html"), name="logout"),
 ]
+
+if settings.DEBUG:
+    # this is for local development serving only, for prod look into django doc
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
